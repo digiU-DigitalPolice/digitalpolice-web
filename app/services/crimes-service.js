d2f5ab989fc0f24c523fd87e8921ce0dbc2b4ee1
Vitalii Kravets <vitalii.kravets@gmail.com>
2016-07-24 13:28:33 +0200

    Fixed multiple requests on zoom bug. Fixes #11.

--- app/services/crimes-service.js
+++ app/services/crimes-service.js
@@ -1,56 +1,59 @@
 'use strict';
 
 angular.module('myApp.services')
-    .service("CrimesService", ['$http', 'ENV', function ($http, ENV) {
+    .service("CrimesService", ['$http', 'ENV', 'MapService', function ($http, ENV, MapService) {
         var self = this;
 
-        self.map = {};
-        self.osmLayer = {};
+        self.categoryIds = [];
+        self.regionIds = [];
+        self.dateFrom = new Date("2014/01/01");
+        self.dateTo = new Date("2017/01/01");
 
-        self.initMap = function () {
-            self.map = new L.Map('map');
-
-            // create the tile layer with correct attribution
-            var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-            self.osmLayer = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});
-
-            // start the map in South-East England
-            self.map.setView([49.8327786, 23.9420238], 11);
-            self.map.addLayer(self.osmLayer);
-            return map;
+        self.setCategoryIds = function (categoryIds) {
+            self.categoryIds = categoryIds;
         };
 
-        self.onEachFeature = function (feature, layer) {
-            if (feature.properties) {
-                layer.bindPopup("count: " + feature.properties.count);
+        self.setRegionIds = function (regionIds) {
+            self.regionIds = regionIds;
+        };
+
+        self.setDateFrom = function (dateFrom) {
+            self.dateFrom = dateFrom;
+        };
+
+        self.setDateTo = function (dateTo) {
+            self.dateTo = dateTo;
+        };
+
+        self.loadCrimes = function () {
+            var params = {
+                dateFrom: new Date(self.dateFrom).getTime(),
+                dateTo: new Date(self.dateTo).getTime()
+            };
+            if (self.categoryIds.length > 0) {
+                params.categories = self.categoryIds;
+            } else {
+                params.categories = [0];
             }
-        };
+            if (self.regionIds.length > 0) {
+                params.regions = self.regionIds;
+            }
 
-        self.renderCrimes = function (params) {
-
-            var bounds = self.map.getBounds();
+            var bounds = MapService.map.getBounds();
 
             params['southWest.latitude'] = bounds._southWest.lat,
                 params['southWest.longitude'] = bounds._southWest.lng,
                 params['northEast.latitude'] = bounds._northEast.lat,
                 params['northEast.longitude'] = bounds._northEast.lng,
-                params['precision'] = self.map.getZoom() - 7;
+                params['precision'] = MapService.map.getZoom() - 7;
 
             $http({
                 url: ENV.apiURL + '/crimes',
                 method: 'GET',
                 params: params
             }).
-            success(function (data, status, headers, config) {
-                self.map.eachLayer(function (layer) {
-                    if (self.osmLayer !== layer) {
-                        self.map.removeLayer(layer);
-                    }
-                });
-                L.geoJson(data, {
-                    onEachFeature: self.onEachFeature
-                }).addTo(self.map);
+            success(function (geoJSON, status, headers, config) {
+                MapService.renderMarkers(geoJSON);
             });
         };
 
