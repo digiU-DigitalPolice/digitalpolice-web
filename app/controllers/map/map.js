2648a9d38d55df916a65e9e8ac3d8a39a30ec036
Yaroslav Liakh <liakh_ya@mail.ru>
2016-08-10 23:05:08 +0300

    3 criteria

--- app/controllers/map/map.js
+++ app/controllers/map/map.js
@@ -2,23 +2,29 @@
 
 angular.module('myApp.map', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/map', {
-    templateUrl: 'controllers/map/map.html',
-    controller: 'MapCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/map', {
+            templateUrl: 'controllers/map/map.html',
+            controller: 'MapCtrl'
+        });
+    }])
 
-.controller('MapCtrl', ['$scope', 'CrimesService', 'MapService', function($scope, CrimesService, MapService) {
+    .controller('MapCtrl', ['$scope', 'CrimesService', 'MapService', function ($scope, CrimesService, MapService) {
+        var $timer;
+        MapService.initMap();
 
-  MapService.initMap();
+        MapService.map.on('zoomend', function (e) {
+            $timeout.cancel($timer);
+            $timer = $timeout(function () {
+                CrimesService.loadCrimes();
+            }, 500);
+        });
 
-  MapService.map.on('zoomend', function (e) {
-      CrimesService.loadCrimes();
-  });
+        MapService.map.on('moveend', function (e) {
+            $timeout.cancel($timer);
+            $timer = $timeout(function () {
+                CrimesService.loadCrimes();
+            }, 500);
+        });
 
-  MapService.map.on('moveend', function (e) {
-      CrimesService.loadCrimes();
-  });
-
-}]);
+    }]);
